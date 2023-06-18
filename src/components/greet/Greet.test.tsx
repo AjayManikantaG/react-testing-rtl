116ff216188afd49fdb82b9240265207e9b6ac09
Ajay Manikanta <39940142+AjayManikantaG@users.noreply.github.com>
2023-06-18 06:22:40 +0000

    Update Greet.test.tsx and Greet.tsx

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,6 +1,16 @@
-import {screen, render} from '@testing-library/react';
-import Greet from './Greet.tsx';
+import { screen, render } from "@testing-library/react";
+import Greet from "./Greet.tsx";
 
-describe('Render Greet component correctly', () => {
-    
-})
+describe("Render Greet component correctly", () => {
+  test("Greets Hello", () => {
+    render(<Greet />);
+    const headingText = screen.getByText("Hello");
+    expect(headingText).toBeInTheDocument();
+  });
+
+  test("Greet Hello along with props name", () => {
+    render(<Greet name="Ajay" />);
+    const headingText = screen.getByRole("heading", { level: 1 });
+    expect(headingText).toHaveTextContent(/Hello Ajay/i);
+  });
+});
--- src/components/greet/Greet.tsx
+++ src/components/greet/Greet.tsx
@@ -1,5 +1,11 @@
-const Greet = () => {
-  return <h1>Hello</h1>;
+type GreetProps = {
+  name?: string;
+};
+
+const Greet = (props: GreetProps) => {
+  const { name } = props;
+
+  return <h1>Hello {name}</h1>;
 };
 
 export default Greet;
